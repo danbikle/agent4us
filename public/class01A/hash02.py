518a894485c06cf27aef271a4eab0df530eec124
Dan Bikle <danbikle@outlook.com>
2017-12-28 00:25:54 -0800

    more

---
+++ public/class01A/hash02.py
@@ -0,0 +1,12 @@
+"""
+hash02.py
+
+This script should demo-how-to operate a popular Python implementation
+of a Cryptographic Hash Function.
+
+This script should call a function which accepts a string and returns a Cryptographic Hash.
+
+python hash02.py
+"""
+
+'bye'
