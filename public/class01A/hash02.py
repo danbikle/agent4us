d57a318a34220329d61ffc69e33d5b5b493d2a51
Dan Bikle <danbikle@outlook.com>
2017-12-28 01:24:04 -0800

    more

--- public/class01A/hash02.py
+++ public/class01A/hash02.py
@@ -4,9 +4,29 @@ hash02.py
 This script should demo-how-to operate a popular Python implementation
 of a Cryptographic Hash Function.
 
-This script should call a function which accepts a string and returns a Cryptographic Hash.
-
 python hash02.py
 """
+
+from cryptography.fernet import Fernet
+
+# I should generate a privatekey:
+privatekey = Fernet.generate_key()
+
+# I should create a fernet object from the key and cryptography-API:
+fernet = Fernet(privatekey)
+
+# I should declare a msg I want to encrypyt:
+calif_bs = b"Today is Dec 27, 2017 and the weather in California is perfect."
+
+# I should encrypt the message:
+cryptographic_hash = fernet.encrypt(calif_bs)
+
+# I should look at the cryptographic_hash:
+print('cryptographic_hash:')
+print(cryptographic_hash)
+
+# I should decrypt cryptographic_hash:
+print('fernet.decrypt(cryptographic_hash):')
+print(fernet.decrypt(cryptographic_hash))
 
 'bye'
