001afe0381088b55e0ccce406301e30b54c2616d
Dan Bikle <danbikle@outlook.com>
2017-12-27 22:24:37 -0800

    more

--- public/class01A/src/us/cryp4/Main.java
+++ public/class01A/src/us/cryp4/Main.java
@@ -1,3 +1,27 @@
+/*
+This Java app should demo-how-to create a simple hash function.
+
+Solution-Idea:
+  - Get the input string, input_s
+  - Maybe the string is too short
+  - add a 256 bit string to end of input_s 
+  - return first 256 bits of input_s
+
+Note that I can create 256 bits from 32 characters which are 8 bytes each.
+
+Linux Demo:
+
+export JAVA_HOME=${HOME}/jdk
+export PATH=${JAVA_HOME}/bin:${PATH}
+
+cd to_the_right_folder
+
+javac    src/us/cryp4/Main.java
+ls -la   src/us/cryp4/Main.class
+java -cp src us.cryp4.Main
+
+*/
+
 package us.cryp4;
 
 public class Main {
@@ -11,10 +35,22 @@ public class Main {
         256 bits is 32 bytes; I should return the first 32 bytes: */
         return out_s.substring(0,32);
     }
+    
     public static void main(String[] args) {
         String my_in_s = "Today is Dec 27, 2017 and the weather in California is perfect.";
-        String out1_s;
-        out1_s = my_hashf(my_in_s);
+        String out1_s  = my_hashf(my_in_s);
         System.out.println(out1_s);
+	
+        my_in_s = "Boston is (16F) cold!";
+        String out2_s  = my_hashf(my_in_s);
+        System.out.println(out2_s);
+	
+        my_in_s = "NYC is (20F) cold too!";
+        String out3_s  = my_hashf(my_in_s);
+        System.out.println(out3_s);
+	
+        my_in_s = "Sydney looks comfy.";
+        String out4_s  = my_hashf(my_in_s);
+        System.out.println(out4_s);
     }
 }
