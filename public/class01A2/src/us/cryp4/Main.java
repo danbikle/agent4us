e69b95b5f4a39d59774262d882651025a9dd6110
Dan Bikle <danbikle@outlook.com>
2017-12-31 18:25:11 -0800

    more

--- public/class01A2/src/us/cryp4/Main.java
+++ public/class01A2/src/us/cryp4/Main.java
@@ -1,3 +1,17 @@
+/*
+Main.java
+
+This app should start a puzzle using a "Puzzle Friendly"
+Cryptographic Hash function named SHA-256.
+
+The idea is very simple; this script should pick a random integer.
+
+Later a contestant will solve the puzzle by guessing the random integer.
+
+Ref:
+http://cryp4.us/cclasses/class01A#puzzle
+ */
+
 package us.cryp4;
 
 public class Main {
