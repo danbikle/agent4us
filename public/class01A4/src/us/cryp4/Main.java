dc9a47a9ee899638c3f9a01ee4569727c1b155e6
Dan Bikle <danbikle@outlook.com>
2018-01-01 22:51:59 -0800

    more

--- public/class01A4/src/us/cryp4/Main.java
+++ public/class01A4/src/us/cryp4/Main.java
@@ -34,20 +34,19 @@ public class Main {
         byte[] buffer= new byte[8192];
         int count_i;
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
-	
-	// curl https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh > /tmp/Anaconda3-5.0.1-Linux-x86_64.sh
+        
+        // curl https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh > /tmp/Anaconda3-5.0.1-Linux-x86_64.sh
         String fileName = "/tmp/Anaconda3-5.0.1-Linux-x86_64.sh";
-	
+        
         BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileName));
         while ((count_i = bis.read(buffer)) > 0) {
             digest.update(buffer, 0, count_i);
         }
-        byte[] hash = digest.digest();
-	/* The call below fails on java version "1.8.0_152":
-        System.out.println(new Base64.getEncoder().encode(hash));
-        The above call should work on Java 8 but fails:
-        Ref:
-        https://docs.oracle.com/javase/8/docs/api/java/util/Base64.html#getEncoder--
-	*/
+        byte[] hash_b         = digest.digest();
+        byte[] encoded_hash_b = Base64.getEncoder().encode(hash_b);
+	// I should convert to String so I can see it:
+        System.out.println(new String(encoded_hash_b));
+	// That's how Java encodes the hash for readability.
+	// I prefer how Python encodes the hash.
     }
 }
