8615b888aea8f40e3049fac5c2b173746283d41d
Dan Bikle <danbikle@outlook.com>
2018-01-01 18:10:20 -0800

    more

---
+++ public/class01A/hash_afile.py
@@ -0,0 +1,17 @@
+"""
+~/cryp4/public/class01A/hash_afile.py
+
+This script should calculate a SHA-256 'secure-hash' of a large file. 
+
+Ref:
+http://cryp4.us/cclasses/class01A#compare2
+https://www.google.com/search?q=how+to+Use+Python+to+get+calculate+a+SHA-256+hash-digest+of+a+large+file
+https://gist.github.com/aunyks/042c2798383f016939c40aa1be4f4aaf
+
+Demo:
+python hash_afile.py
+"""
+
+import hashlib
+
+'bye'
