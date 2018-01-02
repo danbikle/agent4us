7d9905d6742b3cf8c8beb2bb4852c953d6133065
Dan Bikle <danbikle@outlook.com>
2018-01-02 14:31:37 -0800

    more

--- public/class01A1/src/com/mkyong/test/Main.java
+++ public/class01A1/src/com/mkyong/test/Main.java
@@ -9,35 +9,23 @@ package com.mkyong.test;
 
 import java.security.MessageDigest;
 
-/* public class SHAHashingExample
-   I prefer the name: "Main": */
 public class Main
 {
-    public static void main(String[] args)throws Exception
+    public static void main(String[] args) throws Exception
     {
         String password = "123456";
 
         MessageDigest md = MessageDigest.getInstance("SHA-256");
+        
         md.update(password.getBytes());
 
-        byte byteData[] = md.digest();
-
-        //convert the byte to hex format method 1
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < byteData.length; i++) {
-            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-        }
-
-        System.out.println("Hex format : " + sb.toString());
-
-        //convert the byte to hex format method 2
-        StringBuffer hexString = new StringBuffer();
-        for (int i=0;i<byteData.length;i++) {
-            String hex=Integer.toHexString(0xff & byteData[i]);
-            if(hex.length()==1) hexString.append('0');
-            hexString.append(hex);
-        }
-        System.out.println("Hex format : " + hexString.toString());
+        byte hash_b[] = md.digest();
+        
+        // I should convert to String so I can see it:
+        String hash_s = javax.xml.bind.DatatypeConverter.printHexBinary(hash_b);
+        // s.b. 8D969EEF6ECAD3C29A3A629280E686CF0C3F5D5A86AFF3CA12020C923ADC6C92
+        
+        System.out.println("Hex format : " + hash_s);
     }
 }
 
