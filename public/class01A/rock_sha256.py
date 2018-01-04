2dbfac447954d76172ce841c696d65b60f0ea030
Dan Bikle <danbikle@outlook.com>
2018-01-03 23:39:12 -0800

    more

--- public/class01A/rock_sha256.py
+++ public/class01A/rock_sha256.py
@@ -14,8 +14,22 @@ python rock_sha256.py
 import hashlib
 import secrets
 
-def rock_sha256(msg):
-    return 123,True
+def rock_sha256(msg_s):
+    # I should start sha creation by specifying a range of integers.
+    # If the range is large, the puzzle will be difficult.
+    range_i  = int(1e+9)
+    # I should pick a random number from the range:
+    random_i = secrets.choice(range(range_i))
+    # I should convert the number to a string and concatenate it with msg
+    random_s     = str(random_i)
+    random_msg_s = random_s + msg_s
+    # I should generate the hash:
+    my_hashlib = hashlib.sha256()
+    my_hashlib.update(random_msg_s.encode('utf-8'))
+    # I should convert it to a readable string:
+    secret_hash_s = my_hashlib.hexdigest().upper()
+    # random_i and secret_hash_s should be able to verify msg_s:
+    return msg_s, random_i, secret_hash_s
 
 secret_msg = "paper";
 
@@ -23,6 +37,9 @@ print('Although secret_msg never changes, the commitment should change so the Ad
 
 print(rock_sha256(secret_msg))
 print(rock_sha256(secret_msg))
+
+secret_msg = "scissors";
+
 print(rock_sha256(secret_msg))
 print(rock_sha256(secret_msg))
 
