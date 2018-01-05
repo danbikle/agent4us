a4034358c5fe4a885317195a2f4f9ea248810c0c
Dan Bikle <danbikle@outlook.com>
2018-01-05 02:20:13 -0800

    more

--- public/class01A6/src/us/cryp4/Main.java
+++ public/class01A6/src/us/cryp4/Main.java
@@ -15,16 +15,18 @@ import java.security.MessageDigest;
 
 public class Main {
 
-    public static Boolean rock_isvalid(String[] in_a) throws Exception
+    public static Boolean rock_isvalid(String random_s, String msg_s, String secure_hash_s) throws Exception
     {
+	// This function should validate that SHA-256(r|msg) matches a given secure-hash.
 	return true;
     }
+    
     public static void main(String[] args) throws Exception
     {
-	String random_s      = "";
-	String msg_s         = "";
-	String secure_hash_s = "";
-	String[] in_a = {random_s, msg_s, secure_hash_s};
-	rock_isvalid(in_a);
+	// I should operate rock_isvalid():
+	String random_s      = "4D27D9224144319ADD96435E18A625D6FE3CB808DBB5F8F081DC2710DF6D9E83";
+	String msg_s         = "paper";
+	String secure_hash_s = "98065F86634C6C9A049C88F311767D8C31185F1224558BE37E16B1455E59811C";
+	System.out.println(rock_isvalid(random_s, msg_s, secure_hash_s));
     }    
 }
