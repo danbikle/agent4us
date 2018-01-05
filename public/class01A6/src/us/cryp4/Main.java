50a21f83f1caa7e34357220a23e6fb3905e3a840
Dan Bikle <danbikle@outlook.com>
2018-01-05 02:14:14 -0800

    more

--- public/class01A6/src/us/cryp4/Main.java
+++ public/class01A6/src/us/cryp4/Main.java
@@ -12,3 +12,19 @@ http://cryp4.us/cclases/class01A#rock
 package us.cryp4;
 
 import java.security.MessageDigest;
+
+public class Main {
+
+    public static Boolean rock_isvalid(String[] in_a) throws Exception
+    {
+	return true;
+    }
+    public static void main(String[] args) throws Exception
+    {
+	String random_s      = "";
+	String msg_s         = "";
+	String secure_hash_s = "";
+	String[] in_a = {random_s, msg_s, secure_hash_s};
+	rock_isvalid(in_a);
+    }    
+}
