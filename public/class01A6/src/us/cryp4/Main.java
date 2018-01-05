890034116066b91529a0668e119ec125519e50bc
Dan Bikle <danbikle@outlook.com>
2018-01-05 02:08:23 -0800

    more

---
+++ public/class01A6/src/us/cryp4/Main.java
@@ -0,0 +1,14 @@
+/*
+Main.java
+
+This Java-app should create and
+demonstrate a function which validates that
+SHA-256(r|msg) matches a given secure-hash is True or False.
+
+Ref:
+http://cryp4.us/cclases/class01A#rock
+*/
+
+package us.cryp4;
+
+import java.security.MessageDigest;
