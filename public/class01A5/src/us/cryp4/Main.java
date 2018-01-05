e5b0286dda0838ae24b40c2ecab8957d6d58801a
Dan Bikle <danbikle@outlook.com>
2018-01-04 21:34:15 -0800

    more

--- public/class01A5/src/us/cryp4/Main.java
+++ public/class01A5/src/us/cryp4/Main.java
@@ -12,15 +12,18 @@ http://cryp4.us/cclases/class01A#rock
 package us.cryp4;
 
 import java.security.MessageDigest;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) throws Exception
     {
+	Random rand          = new Random();
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
 	// I should loop through 64 places in a hex integer:
 	for (int int_i = 0; int_i < 6; int_i++){
-	    System.out.println(int_i);
+	    int random_i = rand.nextInt(16);
+	    System.out.println(random_i);
 	}
     }
 }
