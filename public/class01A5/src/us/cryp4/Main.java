c35799db5e73fa06afa8d615c278495fc8768c4e
Dan Bikle <danbikle@outlook.com>
2018-01-04 22:41:39 -0800

    more

--- public/class01A5/src/us/cryp4/Main.java
+++ public/class01A5/src/us/cryp4/Main.java
@@ -18,18 +18,24 @@ public class Main {
 
     public static void main(String[] args) throws Exception
     {
-	Random rand          = new Random();
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-	int hex_size_i       = 64;
+	Random rand       = new Random();
+	int hex_size_i    = 64; // Number of places in hex integer.
 	char[] hex_char_a = new char[hex_size_i];
 	// I should loop through 64 places in a hex integer:
-	int place_i;
-	for (place_i = 0; place_i < hex_size_i; place_i++){
+	for (int place_i = 0; place_i < hex_size_i; place_i++) {
+	    // I should pick a random integer at each place:
 	    int random_i = rand.nextInt(16);
-	    char hex_c = Integer.toHexString(random_i).toUpperCase().toCharArray()[0];
-	    hex_char_a[place_i] = hex_c;
+	    // I should convert random_i to a character 0 through f:
+	    String hex_s = Integer.toHexString(random_i);
+	    // I should put char-value of random_i at next place:
+	    hex_char_a[place_i] = hex_s.toCharArray()[0];
 	}
-	String random_s = new String(hex_char_a);
+	// I should now have 256 bit random-int.
+	// I should convert it to readable string:
+	String random_s = new String(hex_char_a).toUpperCase();
 	System.out.println(random_s);
+	String secret_msg    = "paper";
+	String random_msg_s  = random_s + secret_msg;
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
     }
 }
